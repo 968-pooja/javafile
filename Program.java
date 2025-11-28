8eeff61b23f151587c533c6ff6dcb17184789976
Pooja.Mathpati <91341018+968-pooja@users.noreply.github.com>
2025-11-28 16:58:35 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Good Morning");
-      System.out.println(" Hello, From here Pooja Speaking!!");
+      System.out.println(" Hello, Pooja");
     }
 }
